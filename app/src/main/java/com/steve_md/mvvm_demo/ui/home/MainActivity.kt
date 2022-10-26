9f7ab527d8eb5a110571784a6c8c37778fc87e63
MuindiStephen <stephenmuindi241@gmail.com>
2022-10-26 14:07:43 +0300

    wrote more code

--- app/src/main/java/com/steve_md/mvvm_demo/ui/home/MainActivity.kt
+++ app/src/main/java/com/steve_md/mvvm_demo/ui/home/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.steve_md.mvvm_demo.ui.home
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.steve_md.mvvm_demo.R
+import com.steve_md.mvvm_demo.ui.auth.LoginActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+
+        Handler().postDelayed(
+            {
+                startActivity(Intent(this@MainActivity, LoginActivity::class.java))
+                finish()
+            }, 3000
+        )
     }
 }
