24d0738efc3aa9f90a5491cd4d7669f52ec0fa26
MuindiStephen <stephenmuindi241@gmail.com>
2022-10-26 14:54:12 +0300

    Finished working on retrofit

--- app/src/main/java/com/steve_md/mvvm_demo/data/network/LoginUser.kt
+++ app/src/main/java/com/steve_md/mvvm_demo/data/network/LoginUser.kt
@@ -1,4 +1,13 @@
 package com.steve_md.mvvm_demo.data.network
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object LoginUser {
+    var retrofit = Retrofit.Builder()
+        .baseUrl("https://registration-postgres.herokuapp.com/api/v1/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    val api by lazy { retrofit.create(ApiService::class.java) }
 }
